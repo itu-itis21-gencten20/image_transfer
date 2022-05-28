6b3e44043f46be79c2152719d111fd983026220f
itu-itis21-gencten20 <gencten20@itu.edu.tr>
2022-05-28 12:55:30 +0300

    nano update

--- test_1_send_images.py
+++ test_1_send_images.py
@@ -12,25 +12,29 @@ import time
 import numpy as np
 import cv2
 import imagezmq
+import nanocamera as nano
 
 # Create 2 different test images to send
 # A green square on a black background
 # A red square on a black background
 
 sender = imagezmq.ImageSender(connect_to='tcp://192.168.137.1:5555')
-i = 0
+
+camera =nano.Camera(flip=0, width=1280, height=720, fps=30)
+
 image_window_name = 'From Sender'
-while True:  # press Ctrl-C to stop image sending program
+while camera.isReady():  # press Ctrl-C to stop image sending program
     # Increment a counter and print it's value to console
-    i = i + 1
-    print('Sending ' + str(i))
+    try:
+        image = camera.read()
+    
+        sender.send_image(image_window_name, image)
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
 
-    # Create a simple image
-    image = np.zeros((400, 400, 3), dtype='uint8')
-    green = (0, 255, 0)
-    cv2.rectangle(image, (50, 50), (300, 300), green, 5)
+    camera.release()
+    del camera
 
-    # Add counter value to the image and send it to the queue
-    cv2.putText(image, str(i), (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 4)
-    sender.send_image(image_window_name, image)
-    time.sleep(1)
+    
+    
