8b193188d2aea5bd444490e47bb76f554cae1f40
itu-itis21-gencten20 <gencten20@itu.edu.tr>
2022-05-28 13:38:08 +0300

    up

--- test_1_send_images.py
+++ test_1_send_images.py
@@ -12,29 +12,76 @@ import time
 import numpy as np
 import cv2
 import imagezmq
-import nanocamera as nano
+
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1280,
+    capture_height=720,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
 
 # Create 2 different test images to send
 # A green square on a black background
 # A red square on a black background
 
-sender = imagezmq.ImageSender(connect_to='tcp://192.168.137.1:5555')
 
-camera =nano.Camera(camera_type=0, flip=0, width=720, height=480, fps=30)
 
-image_window_name = 'From Sender'
-while camera.isReady():  # press Ctrl-C to stop image sending program
-    # Increment a counter and print it's value to console
-    try:
-        image = camera.read()
-    
-        sender.send_image(image_window_name, image)
-        time.sleep(1/30)
-    except KeyboardInterrupt:
-        break
+def send_camera_image():
+    image_window_name = 'From Sender'
+    sender = imagezmq.ImageSender(connect_to='tcp://192.168.137.1:5555')
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=0))
+    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if video_capture.isOpened():
+        try:
+            while True:
+                ret_val, frame = video_capture.read()
+                sender.send_image(image_window_name, frame)
+                time.sleep(1/30)
+                # Check to see if the user closed the window
+                # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
+                # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
+                
+               
+                
+        finally:
+            video_capture.release()
+            
+    else:
+        print("Error: Unable to open camera")
 
+
+if __name__ == '__main__':
+    send_camera_image()
+
+        
+        
+   
     
-camera.release()
-del camera
-    
-    
+
+
+
+
