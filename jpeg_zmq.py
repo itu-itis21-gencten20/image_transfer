550fade7f52bd71ffdac0a2c3f61c1cc252efa33
itu-itis21-gencten20 <gencten20@itu.edu.tr>
2022-06-02 23:27:54 +0300

    .

--- jpeg_zmq.py
+++ jpeg_zmq.py
@@ -5,32 +5,37 @@ import traceback
 import socket
 from simplejpeg import encode_jpeg
 
+
 def gstreamer_pipeline(
-    sensor_id=0,
-    capture_width=720,
-    capture_height=480,
-    display_width=720,
-    display_height=480,
-    framerate=60,
-    flip_method=0,
+    # sensor_id=0,
+    # capture_width=720,
+    # capture_height=480,
+    # display_width=720,
+    # display_height=480,
+    # framerate=60,
+    # flip_method=0,
 ):
-    return (
-        "nvarguscamerasrc sensor-id=%d !"
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            sensor_id,
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
+    return '''nvarguscamerasrc sensor_id=1 ! 
+   'video/x-raw(memory:NVMM),width=3264, height=2464, framerate=21/1, format=NV12' ! 
+   nvvidconv flip-method=0 ! 'video/x-raw, width=816, height=616' ! 
+   nvvidconv ! nvegltransform ! nveglglessink -e'''
+    # return (
+    #     "nvarguscamerasrc sensor-id=%d !"
+    #     "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+    #     "nvvidconv flip-method=%d ! "
+    #     "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+    #     "videoconvert ! "
+    #     "video/x-raw, format=(string)BGR ! appsink"
+    #     % (
+    #         sensor_id,
+    #         capture_width,
+    #         capture_height,
+    #         framerate,
+    #         flip_method,
+    #         display_width,
+    #         display_height,
+    #     )
+    # )
 
 
 def send_cam_stream():
@@ -54,11 +59,12 @@ def send_cam_stream():
         except (KeyboardInterrupt):
             pass                            # Ctrl-C was pressed to end program
         except Exception as ex:
-                print('Python error with no Exception handler:')
-                print('Traceback error:', ex)
-                traceback.print_exc()
+            print('Python error with no Exception handler:')
+            print('Traceback error:', ex)
+            traceback.print_exc()
         finally:
             video_capture.release()
+
 
 if __name__ == "__main__":
     send_cam_stream()
