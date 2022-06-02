a4cac106ecf6ea250545e6c5fc905b8fe51224a4
itu-itis21-gencten20 <gencten20@itu.edu.tr>
2022-06-02 20:31:12 +0300

    up

--- _test.py
+++ _test.py
@@ -34,7 +34,7 @@ def show_camera():
 
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
     print(gstreamer_pipeline(flip_method=0))
-    video_capture = cv2.VideoCapture(CAP_V4L2)
+    video_capture = cv2.VideoCapture(0)
     if video_capture.isOpened():
         try:
             while True:
