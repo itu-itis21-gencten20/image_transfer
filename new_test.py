204654cd93967cecbff5320c5321656ef00c87cf
itu-itis21-gencten20 <gencten20@itu.edu.tr>
2022-06-02 16:57:01 +0300

    ..

--- new_test.py
+++ new_test.py
@@ -2,31 +2,23 @@ import base64
 import cv2
 import zmq
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.PUB)
-footage_socket.connect('tcp://192.168.137.224:5555')
+#context = zmq.Context()
+#footage_socket = context.socket(zmq.PUB)
+#footage_socket.connect('tcp://192.168.137.224:5555')
 
 camera = cv2.VideoCapture(0)  # init the camera
 
 
 while True:
     try:
-        grabbed, frame = camera.read()  # grab the current frame
-        #print(grabbed, frame)
-
-        if frame is None:
-            print('Wrong path:', path)
-        else:
-            frame = cv2.resize(frame, dsize=(1280, 720))  # resize the frame
-            pixels.append(img)
+        grabbed, frame = camera.read()
+        if frame:
+            frame = cv2.resize(frame, dsize=(1280, 720)) 
         
         encoded, buffer = cv2.imencode('.jpg', frame)
-        #cv2.imshow('abc', )
         jpg_as_text = base64.b64encode(buffer)
-
-        footage_socket.send(jpg_as_text)
-
+        print(jpg_as_text)
+        #footage_socket.send(jpg_as_text)
     except KeyboardInterrupt:
         camera.release()
-        cv2.destroyAllWindows()
         break
