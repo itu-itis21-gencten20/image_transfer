c1468fd70f18168cd4d817d70cd3972d805d7132
itu-itis21-gencten20 <gencten20@itu.edu.tr>
2022-06-02 23:32:16 +0300

    .

--- jpeg_zmq.py
+++ jpeg_zmq.py
@@ -7,35 +7,35 @@ from simplejpeg import encode_jpeg
 
 
 def gstreamer_pipeline(
-    # sensor_id=0,
-    # capture_width=720,
-    # capture_height=480,
-    # display_width=720,
-    # display_height=480,
-    # framerate=60,
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1280,
+    display_height=720,
+    framerate=24,
     flip_method=0,
 ):
-    return '''nvarguscamerasrc sensor_id=1 ! 
-   'video/x-raw(memory:NVMM),width=3264, height=2464, framerate=21/1, format=NV12' ! 
-   nvvidconv flip-method=0 ! 'video/x-raw, width=816, height=616' ! 
-   nvvidconv ! nvegltransform ! nveglglessink -e'''
-    # return (
-    #     "nvarguscamerasrc sensor-id=%d !"
-    #     "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-    #     "nvvidconv flip-method=%d ! "
-    #     "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-    #     "videoconvert ! "
-    #     "video/x-raw, format=(string)BGR ! appsink"
-    #     % (
-    #         sensor_id,
-    #         capture_width,
-    #         capture_height,
-    #         framerate,
-    #         flip_method,
-    #         display_width,
-    #         display_height,
-    #     )
-    # )
+#     return '''nvarguscamerasrc sensor_id=1 ! 
+#    'video/x-raw(memory:NVMM),width=3264, height=2464, framerate=21/1, format=NV12' ! 
+#    nvvidconv flip-method=0 ! 'video/x-raw, width=816, height=616' ! 
+#    nvvidconv ! nvegltransform ! nveglglessink -e'''
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
 
 def send_cam_stream():
