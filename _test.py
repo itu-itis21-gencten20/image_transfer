89192d22a1af7a5aa6693a40495e00001084c9c7
itu-itis21-gencten20 <gencten20@itu.edu.tr>
2022-06-02 20:25:46 +0300

    up

--- _test.py
+++ _test.py
@@ -3,11 +3,21 @@ import base64
 import time
 
 def gstreamer_pipeline(
-   
+    sensor_id=0,
+    capture_width=1280,
+    capture_height=720,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
 ):
     return (
-        "v4l2src device=/dev/video0 ! image/jpeg ! jpegparse ! nvv4l2decoder mjpeg=1 ! nvvidconv ! video/x-raw, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink"
-        """
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
         % (
             sensor_id,
             capture_width,
@@ -17,15 +27,14 @@ def gstreamer_pipeline(
             display_width,
             display_height,
         )
-        """
     )
 
 
 def show_camera():
 
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline())
-    video_capture = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+    print(gstreamer_pipeline(flip_method=0))
+    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     if video_capture.isOpened():
         try:
             while True:
